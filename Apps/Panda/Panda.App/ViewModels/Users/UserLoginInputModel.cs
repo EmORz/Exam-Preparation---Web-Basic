4d14d510e781b5cfabcaa0964dfedba87318faa5
Emo <eiv.razgrad@gmail.com>
2019-06-09 16:53:13 +0300

    user controller

---
+++ Apps/Panda/Panda.App/ViewModels/Users/UserLoginInputModel.cs
@@ -0,0 +1,15 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+
+namespace Panda.App.ViewModels.Users
+{
+    public class UserLoginInputModel
+    {
+        private const string ErrorMessage = "Invalid username or password!";
+
+        [RequiredSis(ErrorMessage)]
+        public string Username { get; set; }
+
+        [RequiredSis(ErrorMessage)]
+        public string Password { get; set; }
+    }
+}
---
+++ Apps/Panda/Panda.App/ViewModels/Users/UserRegisterInputModel.cs
@@ -0,0 +1,26 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+
+namespace Panda.App.ViewModels.Users
+{
+    public class UserRegisterInputModel
+    {
+        private const string UsernameErrorMessage = "Invalid username length! Must be between 5 and 20 symbols!";
+
+        private const string PasswordErrorMessage = "Invalid password length!";
+
+        [RequiredSis]
+        [StringLengthSis(5, 20, UsernameErrorMessage)]
+        public string Username { get; set; }
+
+        [RequiredSis]
+        [PasswordSis(nameof(ConfirmPassword))]
+        public string Password { get; set; }
+
+        [RequiredSis]
+        public string ConfirmPassword { get; set; }
+
+        [RequiredSis]
+        [EmailSis]
+        public string Email { get; set; }
+    }
+}
